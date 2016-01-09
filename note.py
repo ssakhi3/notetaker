c689ee752307ee14cb039cb68c071971cd232814
Sidra Sakhi <sidrasakhi@gatech.edu>
2016-01-08 20:40:38 -0500

    fixed note class and wrote json for notes

--- note.py
+++ note.py
@@ -1,37 +1,43 @@
-
+from datetime import datetime as dt
 
 class Note(object):
 
-	def __init__(self, title, date_created, date_modified,  content,  userId,  noteId= None):
+	def __init__(self, title, content,  userId, date_created=dt.now(), date_modified=dt.now(), noteId=None):
+		
 		if not all([title, content, userId]):
 			raise ValueError
 
-
 		self.title = title
-		self.date_created = date_created
-		self.date_modified = date_modified
 		self.content = content
 		self.userId = userId
 		self.noteId = noteId
 
+		self.date_created = date_created
+		self.date_modified = date_modified
+
 	def create_json(self): 
 		mynote = dict()
 		mynote["title"] = sef.title
-		mynote["date_created"] = self.date_created
-		mynote["date_modified"] = self.date_modified
 		mynote["content"] = self.content
 		mynote["userId"] = self.userId
 		mynote["noteId"] = self.noteId
+
+		mynote["date_created"] = self.date_created
+		mynote["date_modified"] = self.date_modified
 
 		return mynote
 
 	@classmethod
 	def read_json(constructor, mynote):
 		title = mynote.get("title")
-		date_created = mynote.get("date_created")
-		date_modified = mynote.get("date_modified")
 		content = mynote.get("content")
 		userId = mynote.get("userId")
 		noteId = mynote.get("noteId")
 
-		return constructor(title, date_created, date_modified, content, userId, noteId)
+		date_created = mynote.get("date_created")
+		date_modified = mynote.get("date_modified")
+
+		return constructor(title, content,  userId, date_created, date_modified, noteId)
+
+	def update_time(self):
+		self.date_modified = dt.now()
