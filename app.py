91e784a8b0fd730c0e2e7d47aad8dd64b00406cd
ssakhi <sidrasakhi@gatech.edu>
2016-09-18 12:17:33 -0400

    Index page shows shared notes

--- app.py
+++ app.py
@@ -6,15 +6,24 @@ import json
 app = Flask('MyApp')
 
 @app.route('/')
-@app.route('/signup')
-def signup():
-	return render_template('signup.html')
+def index():
+	notesJson = list(db.notes.find({}))
+	notes = map(Note.from_json, notesJson)
+	note_titles = []
+	contents = []
+	noteIds = []
+	for note in notes: 
+		note_titles.append(note.title)
+		contents.append(note.content)
+		noteIds.append(note.noteId)
+
+
+	return render_template('show_notes.html', titles=note_titles, noteIds=noteIds)
 
 #if note id, find single note
 # if no note id, find all notes
 #if multiple note ids, show all
 @app.route('/notes/<int:noteId>')
-@app.route('/notes/')
 def show_notes(noteId=None):
 	if noteId:
 		notes = (list(db.notes.find({'noteId': noteId})))
@@ -25,22 +34,6 @@ def show_notes(noteId=None):
 			content = note.content
 			noteId = note.noteId
 			return render_template('show_note.html', title=note_title, content=content, noteId=noteId)
-
-		# Go somewhere else
-		return signup()
-	else:
-		notesJson = list(db.notes.find({}))
-		notes = map(Note.from_json, notesJson)
-		note_titles = []
-		contents = []
-		noteIds = []
-		for note in notes: 
-			note_titles.append(note.title)
-			contents.append(note.content)
-			noteIds.append(note.noteId)
-
-
-		return render_template('show_notes.html', titles=note_titles, noteIds=noteIds)
 
 
 @app.route('/update_note', methods=["POST"])
@@ -76,4 +69,4 @@ def new_note():
 
 
 app.debug = True
-app.run()
+app.run(port=8080)
